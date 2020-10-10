3f1f80fa9e0699393a28815d5687439c72d6b129
codewheezy <uchennaibe95@gmail.com>
2020-10-10 20:24:17 +0530

    added change to the new file

--- image.py
+++ image.py
@@ -1,16 +1,8 @@
 from PIL import Image, ImageFilter
 
 img = Image.open('./Pokedex/pikachu.jpg')
-filtered_img = img.filter(ImageFilter.BLUR)
-# filtered_img = img.filter(ImageFilter.SHARPEN)
-# filtered_img = img.filter(ImageFilter.SMOOTH)
 filtered_img = img.convert('L')
-# rotate the image
-# crooked = filtered_img.rotate(120)
-# resize the Image
-resize = filtered_img.resize((300, 300))
-# save the image
-resize.save("grey.png", 'png')
-# show the image
-# filtered_img.show()
-resize.show()
+box = (100, 100, 400, 400)
+region = filtered_img.crop(box)
+region.save("grey.png", 'png')
+region.show()
