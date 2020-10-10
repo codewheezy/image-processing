2f7331aa4b6215b79c8da2af83dfe838a62d1f5c
codewheezy <uchennaibe95@gmail.com>
2020-10-10 14:36:33 +0530

    added newchanges

---
+++ image.py
@@ -0,0 +1,8 @@
+from PIL import Image, ImageFilter
+
+img = Image.open('./Pokedex/pikachu.jpg')
+filtered_img = img.filter(ImageFilter.BLUR)
+# filtered_img = img.filter(ImageFilter.SHARPEN)
+# filtered_img = img.filter(ImageFilter.SMOOTH)
+filtered_img = img.convert('L')
+filtered_img.save("grey.png", 'png')
